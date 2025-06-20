9b7db3db4ab8d91ffecea3de2922bd9850f9028a
HENRY DON 🖤🔥 <lukassashy@gmail.com>
2025-06-21 00:34:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,6 @@ app = Flask(__name__)
 
 tasks = {}
 
-    'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
-    'referer': 'www.google.com'
-
-    
 @app.route('/start_task', methods=['POST'])
 def start_task():
     data = request.json
