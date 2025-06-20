9ddb10a78e890a9e8a0c14ed7df00af3cce52a0d
HENRY DON 🖤🔥 <lukassashy@gmail.com>
2025-06-20 11:30:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ headers = {
     'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
     'referer': 'www.google.com'
 }
-
+   return
 @app.route('/', methods=['GET', 'POST'])
 def send_message():
     if request.method == 'POST':
