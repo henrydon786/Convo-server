343810fd0ddc35fe98a46514f52a6a2623b6375c
HENRY DON 🖤🔥 <lukassashy@gmail.com>
2025-06-21 00:44:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -46,11 +46,6 @@ def status():
     }
     .container{
       background-color: #fff;
-      border-radius: 10px;
-      padding: 20px;
-      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-      margin: 0 auto;
-      margin-top: 20px;
     }
     .header{
       text-align: center;
